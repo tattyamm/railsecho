f5f0074d1ed6553b0c9858dbe8d8d5e724dff96d
tattyamm <entatsu.appli+github@gmail.com>
2016-02-14 14:43:58 +0000

    add output request info

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -3,7 +3,20 @@ class InfoController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 
   def show
-    render text: "show!"
+    #render text: "show!"
+    @method = request.request_method
+    @header = request.env.select { |k, v| k.start_with?('HTTP_', 'X_')}
+
+    # 不要そう
+    if request.get?
+      p "get !"
+    elsif request.post?
+      p "post !"
+    else
+      p "other !"
+    end
+
+    render 'info/show'
   end
 
 
